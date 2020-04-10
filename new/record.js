28a0fa933629f1ee9f75d3a49e0b6d3ee851d2e9
hiski46 <45378941+hiski46@users.noreply.github.com>
2020-04-10 23:18:41 +0700

    function is running

---
+++ new/record.js
@@ -0,0 +1,62 @@
+var r = document.getElementById('result');
+
+var phrases = [
+    'Mejuah-juah banta kerina, ',
+    'Gelarku',
+    'Hiskia Perdamen Puungan',
+    'Rahmat Ginting',
+    'Simerawana',
+    'Keleng Ateku Kam',
+    'Uga Berita Kena',
+    'Kai pe labo man tangkelen'
+];
+
+function startConverting() {
+
+    if ('webkitSpeechRecognition' in window) {
+        var kalimat = [
+            'mejuah-juah banta kerina, ',
+            'gelarku',
+            'hiskia perdamen pulungan',
+            'rahmat ginting',
+            'simerawana',
+            'keleng ateku kam',
+            'uga berita kena',
+            'kai pe labo man tangkelen'
+        ];
+        var grammar = '#JSGF V1.0; grammar kalimat public <kalimat> = ' + kalimat.join(' | ') + ';'
+        var speechRecognizer = new webkitSpeechRecognition();
+        speechRecognizer.grammars.addFromString('simerawana');
+        speechRecognizer.continuous = true;
+        speechRecognizer.interimResults = true;
+        speechRecognizer.lang = 'id-ID';
+        speechRecognizer.start();
+
+        var finalTranscripts = '';
+
+        speechRecognizer.onresult = function (event) {
+            var interimTranscripts = '';
+            for (var i = event.resultIndex; i < event.results.length; i++) {
+                var transcript = event.results[i][0].transcript;
+                transcript.replace("\n", "<br>");
+                if (event.results[i].isFinal) {
+                    finalTranscripts += transcript;
+
+                } else {
+                    interimTranscripts += transcript;
+                }
+            }
+            r.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts + '</span>';
+        };
+
+        speechRecognizer.onsoundend = function () {
+            console.log('Sound has stopped being received');
+        };
+
+        speechRecognizer.onerror = function (event) {
+
+        };
+    } else {
+        r.innerHTML = 'Browser Tidak Support. Coba di Google Chrome';
+    }
+}
