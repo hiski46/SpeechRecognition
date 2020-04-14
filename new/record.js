bc88849a23f4e432caadb24c9b0dcc604fd095da
hiski46 <hiskia.14117091@student.itera.ac.id>
2020-04-14 13:54:03 +0700

    perbaikan

--- new/record.js
+++ new/record.js
@@ -1,46 +1,25 @@
 var r = document.getElementById('result');
 
-var phrases = [
-    'Mejuah-juah banta kerina, ',
-    'Gelarku',
-    'Hiskia Perdamen Puungan',
-    'Rahmat Ginting',
-    'Simerawana',
-    'Keleng Ateku Kam',
-    'Uga Berita Kena',
-    'Kai pe labo man tangkelen'
-];
 
-var kalimat = [
-    'mejuah-juah banta kerina, ',
-    'gelarku',
-    'hiskia perdamen pulungan',
-    'rahmat ginting',
-    'simerawana',
-    'keleng ateku kam',
-    'uga berita kena',
-    'kai pe labo man tangkelen'
-];
-var grammar = '#JSGF V1.0; grammar kalimat public <kalimat> = ' + kalimat.join(' | ') + ';'
 var speechRecognizer = new webkitSpeechRecognition();
-speechRecognizer.grammars.addFromString('simerawana');
 speechRecognizer.continuous = true;
 speechRecognizer.interimResults = true;
 speechRecognizer.lang = 'id-ID';
 
 function startConverting() {
 
-    if ('webkitSpeechRecognition' in window) {
-
+    if (!('webkitSpeechRecognition' in window)) {
+        upgrade();
+    } else {
         speechRecognizer.start();
 
-        var finalTranscripts = '';
+        var finalTranscripts = ''
 
         speechRecognizer.onresult = function (event) {
             var interimTranscripts = '';
             for (var i = event.resultIndex; i < event.results.length; i++) {
                 var transcript = event.results[i][0].transcript;
-                transcript.replace("\n", "<br>");
+                transcript.replace("\n", "<p>");
                 if (event.results[i].isFinal) {
                     finalTranscripts += transcript;
 
@@ -48,18 +27,15 @@ function startConverting() {
                     interimTranscripts += transcript;
                 }
             }
-            r.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts + '</span>';
+
+            r.innerHTML = '<span id="finalspan" >' + finalTranscripts + '</span>' + '<span id="interimspan" style="color:#999">' + interimTranscripts + '</span>';
+
         };
 
-        speechRecognizer.onsoundend = function () {
-            console.log('Sound has stopped being received');
+
+        speechRecognizer.onerror = function (event) {
+            console.log('Speech recognition error detected: ' + event.error);
         };
-
-        // speechRecognizer.onerror = function (event) {
-
-        // };
-    } else {
-        r.innerHTML = 'Browser Tidak Support. Coba di Google Chrome';
     }
 }
 
